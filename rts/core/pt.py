a530ec48a3b2a26c49ff1b13fb48ff8cd69ef667
Youngeun Cho <upoque@gmail.com>
2019-01-24 13:08:59 +0900

    Added parallel assignment argument in pt

--- rts/core/pt.py
+++ rts/core/pt.py
@@ -2,21 +2,26 @@ from rts.core.task import Task
 from rts.core.ts import TaskSet
 from rts.op import para
 
+
 class ParaTask(object):
     'Parallelizable Task Set'
     cnt = 0
 
     def __init__(self, **kwargs):
+        type(self).cnt += 1
         self.id = kwargs.get('id', type(self).cnt)
         self.max_opt = kwargs.get('max_option', 1)
 
+        # base task info
         self.base_task = kwargs.get('base_task', Task(**{'exec_time': 1, 'deadline': 2, 'period': 3}))
         ts = TaskSet()
         ts.append(self.base_task)
-        self.ts_table = {
-            '1': ts
-        }
-        type(self).cnt += 1
+        self.ts_table = {'1': ts}
+
+        # parallelizer info
+        self.overhead = kwargs.get('overhead', 0.0)
+        self.variance = kwargs.get('variance', 0.0)
+
         self.populate_ts_table()
 
     def __del__(self):
@@ -51,25 +56,21 @@ class ParaTask(object):
             raise Exception('Parallization option cannot be 0.')
         elif opt <= len(self.ts_table):
             self.ts_table[str(opt)] = ts
-            print('sss')
             return
         else:
             raise Exception('Parllelization option out of bound.\n' +
                             'max_parallel option: ' + str(self.max_opt) + '\n' +
                             'requested option: ' + str(opt))
 
-    def append(self, opt, ts):
-        pass
-
     def populate_ts_table(self):
         if self.max_opt >= 2:
             for i in range(2, self.max_opt + 1):
                 self.ts_table[str(i)] = TaskSet()
-                tlist = para.parallelize_task(self.base_task, **{'pcs': i})
+                tlist = para.parallelize_task(self.base_task, **{'pcs': i, 'overhead': self.overhead, 'variance': self.variance})
                 for thr in tlist:
                     self[i].append(thr)
-
         return
+
 
 if __name__ == '__main__':
     task_param = {
@@ -81,6 +82,8 @@ if __name__ == '__main__':
     para_task_param = {
         'base_task': t,
         'max_option': 4,
+        'overhead': 0.0,
+        'variance': 0.3,
     }
     pt = ParaTask(**para_task_param)
     print(pt)
