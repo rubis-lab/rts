4b123df64ed0b002320e7b940681819e09db37bb
Youngeun Cho <upoque@gmail.com>
2019-01-24 17:59:38 +0900

    Edited parallel task sets

--- rts/core/pts.py
+++ rts/core/pts.py
@@ -31,12 +31,6 @@ class ParaTaskSet(object):
         self.serialize_pts(**{'popt': self.popt})
         return
 
-        # task set list
-
-        # ????
-        self.thr_list_seq = []
-        self.thr_cnt = [0]
-
     def __del__(self):
         type(self).cnt -= 1
 
@@ -45,54 +39,25 @@ class ParaTaskSet(object):
                'max_option: ' + str(self.max_opt) + '\n\n' + \
                'base_ts: ' + '\n' + str(self.base_ts) + '\n' + \
                'generated: \n' + str(self.pts_serialized)
-        #for t in self.pts_serialized:
-        #    info += str(t) + '\n'
 
         return info
 
     def __len__(self):
-        return len(self.thr_list_seq)
+        return len(self.pts_serialized)
 
     def __getitem__(self, idx):
-        return self.thr_list_seq[self.thr_cnt[idx]:self.thr_cnt[idx + 1]]
+        return self.pts_serialized[idx]
+
+    def __setitem__(self, idx, thr):
+        self.pts_serialized[idx] = thr
+        return
 
     def __iter__(self):
-        return iter(self.thr_list_seq)
-
-    def shift_thr_cnt_right(self, t_idx):
-        for curr_t_idx in range(len(self.thr_cnt)):
-            if curr_t_idx > t_idx:
-                self.thr_cnt[curr_t_idx] += 1
-        return
-
-    def shift_thr_cnt_left(self, t_idx):
-        for curr_t_idx in range(len(self.thr_cnt)):
-            if curr_t_idx > t_idx:
-                self.thr_cnt[curr_t_idx] -= 1
-        return
-
-    def append(self, t, t_idx=-1):
-        # insert list of items
-        if t_idx < 0:
-            self.thr_list_seq[len(self.thr_list_seq):] = t
-            self.thr_cnt.append(self.thr_cnt[-1] + len(t))
-
-        # insert single item
-        else:
-            # if sibling thread already exist in list
-            if t_idx + 1 < len(self.thr_cnt):
-                self.thr_list_seq.insert(self.thr_cnt[t_idx + 1], t)
-                self.shift_thr_cnt_right(t_idx)
-
-            # if sibling thread not in list
-            else:
-                self.thr_list_seq.append(t)
-                while t_idx + 1 > len(self.thr_cnt):
-                    self.thr_cnt.append(self.thr_cnt[-1])
-                self.thr_cnt.append(self.thr_cnt[-1] + 1)
+        return iter(self.pts_serialized)
 
     def clear(self):
-        del self.thr_list_seq[:]
+        del self.pts_serialized[:]
+        del self.pt_list[:]
         return
 
     def populate_pt_list(self):
--- tests/unit/core/test_pts.py
+++ tests/unit/core/test_pts.py
@@ -6,23 +6,6 @@ import unittest
 
 class PTSTestCase(unittest.TestCase):
     """ Tests for `pts.py`."""
-    def test_thr_added(self):
-        thr11 = Thread()
-        thr12 = Thread()
-
-        thr_list1 = []
-        thr_list1.append(thr11)
-        thr_list1.append(thr12)
-
-        thr21 = Thread()
-        thr_list2 = []
-        thr_list2.append(thr21)
-
-        pts = ParaTaskSet()
-        pts.append(thr_list1)
-        pts.append(thr_list2)
-
-        self.assertEqual(len(pts), 3)
 
     def test_get_thr(self):
         thr11 = Thread()
