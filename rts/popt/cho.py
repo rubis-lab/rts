815f9db075e3d37c5cb12e7c541bf5aaa6246308
Youngeun Cho <upoque@gmail.com>
2019-02-07 16:01:26 +0900

    Implemented ip table, para needs check

--- rts/popt/cho.py
+++ rts/popt/cho.py
@@ -4,10 +4,11 @@ from rts.core.task import Task
 from rts.core.ts import TaskSet
 from rts.core.pts import ParaTaskSet
 from rts.sched.bcl_naive import BCL_Naive
+from rts.op import tsutil
 
 
-class Exhaustive(Popt):
-    'Exhaustive search'
+class Cho(Popt):
+    'Cho 2019'
 
     def __init__(self, **kwargs):
         self.max_opt = kwargs.get('max_option', 1)
@@ -21,32 +22,72 @@ class Exhaustive(Popt):
         info = ''
         return info
 
-    def conv_num_base(self, n, b, idx):
-        if n == 0:
-            return idx
-        i = 0
-        while n:
-            idx[i] = int(n % b)
-            i += 1
-            n //= b
-        return idx
+    def create_inter_vs_popt_table(self, pts):
+        self.ip_table = []
+        n_task = len(pts.base_ts)
+        for i in range(n_task):
+            self.ip_table.append([])
+            for j in range(self.max_opt):
+                # thread set of task i, option j + 1
+                thrs_i_oj = pts.pt_list[i][j + 1]
+
+                # Minimum laxity (D - C) among thread set
+                lax = []
+                for k in range(len(thrs_i_oj)):
+                    lax.append(thrs_i_oj[k].deadline - thrs_i_oj[k].exec_time)
+
+                print('thrs_i list')
+                print(i)
+                print(thrs_i_oj)
+                print('lax list')
+                print(lax)
+                print('min lax value')
+                print(min(lax))
+                print('min lax idx')
+                print(lax.index(min(lax)))
+                print('corresponding thr')
+                print(thrs_i_oj[lax.index(min(lax))])
+
+                self.ip_table[i].append(min(lax))
+        return
 
     def is_schedulable(self, pts):
+        # Create interference vs parallel option table for every task
+        self.create_inter_vs_popt_table(pts)
+        print('ip_table')
+        print(self.ip_table)
+
+        # Initial - all tasks at lowest parallelization
+        pts.popt_strategy = 'single'
+        pts.serialize_pts()
+
+        n_task = len(pts.base_ts)
+        for i in range(n_task):
+            pass
+
+        """
         # number of possible cases: max_opt ^ n_task
         n_task = len(pts.base_ts)
         for i in range(self.max_opt ** n_task):
+            #print('i')
+            #print(i)
             idx = [0 for j in range(n_task)]
+            #print('idx: ')
             self.conv_num_base(i, self.max_opt, idx)
+            #print(idx)
 
             # set parallel option
             for j in range(n_task):
                 pts.popt_list[j] = idx[j] + 1
             pts.serialize_pts()
+            #print(pts)
 
             # check schedulability
             if self.sched.is_schedulable(pts):
+                #print('True')
                 return True
-
+            #print('False')
+        """
         return False
 
     def get_opt_popt(self, pts):
@@ -74,7 +115,7 @@ if __name__ == '__main__':
 
     pts_param = {
         'base_ts': ts,
-        'max_option': 2,
+        'max_option': 4,
         'overhead': 0.0,
         'variance': 0.3,
         'popt_strategy': 'custom',
@@ -90,10 +131,10 @@ if __name__ == '__main__':
     bcl_naive = BCL_Naive(**sched_param)
 
     popt_param = {
-        'max_option': 2,
+        'max_option': 4,
         'sched':bcl_naive,
     }
-    exhaustive = Exhaustive(**popt_param)
+    exhaustive = Cho(**popt_param)
 
     ss = exhaustive.is_schedulable(pts)
     print('Exhaustive test result:')
--- rts/sched/bcl_naive.py
+++ rts/sched/bcl_naive.py
@@ -1,35 +1,14 @@
 import math
 from rts.sched.sched import Sched
+from rts.op import tsutil
 
 
 class BCL_Naive(Sched):
     def __init__(self, **kwargs):
         self.num_core = float(kwargs.get('num_core', 1.0))
 
-    def calc_whole_inclusion(self, base_task, inter_task):
-        n_inter_task = math.floor(base_task.deadline / inter_task.period)
-
-        return n_inter_task * inter_task.exec_time
-
-    def calc_carry_in_naive(self, base_task, inter_task):
-        # carry-ins calculated from slack values
-        carry_in = math.fmod(base_task.deadline, inter_task.period)
-
-        # carry-in has to be positive
-        if carry_in < 0.0:
-            carry_in = 0.0
-
-        # carry-in cannot exceed the actual execution time
-        return min(inter_task.exec_time, carry_in)
-
     def calc_interference(self, base_task, inter_task):
-        i_sum = 0.0
-
-        # threads aligned to deadlines
-        i_sum += self.calc_whole_inclusion(base_task, inter_task)
-
-        # carry-ins
-        i_sum += self.calc_carry_in_naive(base_task, inter_task)
+        i_sum = tsutil.workload_in_interval_edf(inter_task, base_task.deadline)
 
         # interference is limited to leftover of basetask
         i_sum = min(i_sum, base_task.deadline - base_task.exec_time + 1.0)
